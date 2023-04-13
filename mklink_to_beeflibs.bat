d84c1ef4515e92fd09e62c778f30ec6d33b58226
maihd <maihd.dev@gmail.com>
2023-04-13 19:56:35 +0700

    work on mklink

---
+++ mklink_to_beeflibs.bat
@@ -0,0 +1,26 @@
+@echo off
+
+set BEEFLANG_PATH=C:\Program Files\BeefLang
+set BEEFLIBS_PATH=%BEEFLANG_PATH%\BeefLibs
+
+if not exist "%BEEFLANG_PATH%" (
+    echo Beef not found, please sure you have installed Beef, or change BEEFLANG_PATH above
+    goto :end
+)
+
+if not exist "%BEEFLIBS_PATH%" (
+    echo BeefLibs not found, please sure you have installed Beef, or change BEEFLANG_PATH above
+    goto :end
+)
+
+if exist "%BEEFLIBS_PATH%\raylib" (
+    echo Overwriting "%BEEFLIBS_PATH%\raylib"
+    rmdir /s /q "%BEEFLIBS_PATH%\raylib"
+    del "%BEEFLIBS_PATH%\raylib"
+    goto :copy_to_beeflibs
+)
+
+mklink . "%BEEFLIBS_PATH%\raylib"
+
+:end
+echo Done
